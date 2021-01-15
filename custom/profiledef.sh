1e6419852e38d0ba397facb618e29e71a87645a9
Titux Metal <github@lgdweb.fr>
2021-01-15 08:34:26 +0100

    Adding profiledef.sh file in custom; profile definition for the iso

---
+++ custom/profiledef.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="customarchlinux-xfce"
+iso_label="custom-archlinux_$(date +%Y%m)"
+iso_publisher="Custom Arch Linux <https://github.com/custom-archlinux>"
+iso_application="Custom Arch Linux Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
