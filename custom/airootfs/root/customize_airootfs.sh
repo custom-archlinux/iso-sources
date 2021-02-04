ccd6c0afc9ecfb92f0e19caa804dbb3857d32782
Titux Metal <github@lgdweb.fr>
2021-02-04 11:12:32 +0100

    Removing locale.gen sed command line from customize_airootfs file; this is for locale configuration that is already present in the original archiso

--- custom/airootfs/root/customize_airootfs.sh
+++ custom/airootfs/root/customize_airootfs.sh
@@ -6,7 +6,4 @@ set -e -u
 
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
-locale-gen
-
 cp -aT /etc/skel/ /root/
