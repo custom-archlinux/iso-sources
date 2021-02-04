33317574978a69936c8346aac46e63849a0b9cda
Titux Metal <github@lgdweb.fr>
2021-02-04 10:17:53 +0100

    Removing logind.conf sed command lines from customize_airootfs file; adding logind.conf file in etc/systemd

--- custom/airootfs/root/customize_airootfs.sh
+++ custom/airootfs/root/customize_airootfs.sh
@@ -14,10 +14,6 @@ cp -aT /etc/skel/ /root/
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 
-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
-
 systemctl enable pacman-init.service choose-mirror.service systemd-networkd.service systemd-resolved.service
 systemctl set-default graphical.target
 # systemctl enable reflector.service
