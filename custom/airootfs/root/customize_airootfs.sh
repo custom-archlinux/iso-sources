bdcdf9898890c36ae4b4c34812048d28fe09e784
Titux Metal <github@lgdweb.fr>
2021-07-04 09:25:50 +0200

    Removing airootfs directory in custom; not needed anymore

--- custom/airootfs/root/customize_airootfs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-# set -e -u
-
-# Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
-
-# cp -aT /etc/skel/ /root/
