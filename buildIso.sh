cf37b8bac3182a86feb75a688a82d6f459949808
Titux Metal <github@lgdweb.fr>
2021-07-04 09:30:42 +0200

    Moving the build script in main directory; this is the script to build the iso image

--- buildIso.sh
+++ buildIso.sh
@@ -1,22 +1,98 @@
 #!/bin/bash
-set -e
 
-echo "Cleanup the archiso directory"
-[ -d archiso/ ] && rm -rf archiso
+# Name: buildIso.sh
+# Description: Build an ISO image of ALICE base on custom settings
+# Author: Tuxi Metal <tuximetal[at]lgdweb[dot]fr>
+# Url: https://github.com/custom-archlinux/iso-sources
+# Version: 1.0
+# Revision: 2021.06.27
+# License: MIT License
 
-mkdir archiso
+workspace="$HOME/ALICE-workspace"
+sourcesDir="$(pwd)/archiso/"
+customFiles="$(pwd)/custom/"
+outDirectory="$(pwd)/out/"
+workDirectory="$(pwd)/work/"
+logFile="$(pwd)/$(date +%T).log"
 
-echo "Copy archiso files from the system to the local archiso directory"
-cp -R /usr/share/archiso/configs/releng/* archiso/
+# Helper function for printing messages $1 The message to print
+printMessage() {
+  message=$1
+  tput setaf 2
+  echo "-------------------------------------------"
+  echo "$message"
+  echo "-------------------------------------------"
+  tput sgr0
+}
 
-echo "Copy custom files in archiso"
-cp -rf custom/* archiso/
+# Helper function to handle errors
+handleError() {
+  clear
+  set -uo pipefail
+  trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+}
 
-echo "Change the owner to root"
-chown -R root:root archiso
+isRootUser() {
+  if [[ ! "$EUID" = 0 ]]; then 
+    printMessage "Please Run As Root"
+    exit 0
+  fi
+  printMessage "Ok to continue running the script"
+  sleep .5
+}
 
-cd archiso
+changeOwner() {
+  newOwner=$1
+  directoryName=$2
+  printMessage "Change owner of ${directoryName} to ${newOwner}"
+  chown -R ${newOwner} ${directoryName}
+  sleep .5
+}
 
-echo "Launch the build.sh script"
-echo "Change the owner back to the current user"
-sh build.sh && chown -R 1000:1000 ./*
+removeDirectory() {
+  dirName=$1
+  printMessage "Cleanup ${dirName} directory"
+  [ -d ${dirName}/ ] && rm -rf ${dirName}
+}
+
+copyFiles() {
+  source=$1
+  target=$2
+  message=$3
+  printMessage "${message}"
+  cp -R ${source} ${target}
+  sleep .5
+}
+
+runMkarchiso() {
+  printMessage "Start of building the ISO image"
+  mkarchiso -v -w ${workDirectory} -o ${outDirectory} ${sourcesDir}
+}
+
+# Cleanup before build
+preBuild() {
+  removeDirectory "${sourcesDir}"
+  removeDirectory "${outDirectory}"
+  removeDirectory "${workDirectory}"
+  sleep .5
+  printMessage "Create archiso directory"
+  mkdir -p ${sourcesDir}
+  sleep .5  
+}
+
+main() {
+  handleError
+  isRootUser
+  preBuild
+  copyFiles "/usr/share/archiso/configs/releng/*" ${sourcesDir} "Copy archiso files from the system to the local archiso directory"
+  copyFiles "${customFiles}*" "${sourcesDir}" "Copy custom files in archiso directory"
+  changeOwner "root:root" "${sourcesDir}"
+  runMkarchiso
+  changeOwner "1000:1000" "$(pwd)"
+
+  printMessage "All is done!"
+
+  exit 0
+}
+
+time main
--- custom/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-_buildsh_path="$(realpath -- "$0")"
-exec mkarchiso -v "$@" "${_buildsh_path%/*}"
